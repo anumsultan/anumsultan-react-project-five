72b730b349daffcd39d7ba0f6deab80cb83a3f89
anum <anumsultan94@gmail.com>
2022-09-07 17:10:24 +0500

    first commit

---
+++ Travel-Journal/src/App.js
@@ -0,0 +1,25 @@
+import Header from './Header'
+import Journey from './Journey';
+import data from './data.js'
+
+const places = data.map(function(place){
+  return(
+    <Journey
+      key = {place.key}
+      {...place}
+    />
+  )
+})
+
+function App() {
+  return (
+    <div className="container">
+      <Header/>
+      <div className="grid">
+        {places}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Travel-Journal/src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+export default function Header(){
+    return(
+        <header>
+            <div className="logo">
+                <img src='../images/globe.png' alt="globe" />
+                <p>my travel journal</p>
+            </div>
+        </header>
+    )
+}
